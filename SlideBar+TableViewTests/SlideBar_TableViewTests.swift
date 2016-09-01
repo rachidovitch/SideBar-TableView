0eee20436eb9fc6310430173965ee707792265dd
mac <mac@stefanie.local>
2016-09-01 17:13:50 +0100

    Initial Commit

---
+++ SlideBar+TableViewTests/SlideBar_TableViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  SlideBar_TableViewTests.swift
+//  SlideBar+TableViewTests
+//
+//  Created by mac on 01/09/2016.
+//  Copyright © 2016 Rachidovitch. All rights reserved.
+//
+
+import XCTest
+@testable import SlideBar_TableView
+
+class SlideBar_TableViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
